23007eab970a7e0dad767414f6294287784db793
JinsaGalbi <41094016+uncyclocity@users.noreply.github.com>
2019-12-03 17:53:05 +0900

    로그인 &Than 회원가입 구현완료

--- app/src/main/java/jp/dylee/nodeorder/LoginActivity.java
+++ app/src/main/java/jp/dylee/nodeorder/LoginActivity.java
@@ -3,7 +3,6 @@ package jp.dylee.nodeorder;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,9 +16,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends Activity {
-    private FirebaseAuth firebaseAuth;
+    FirebaseAuth mAuth;
     EditText email, password;
     String em = "", pw = "";
+    Button login, signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,37 +27,46 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.pw);
-        Button login = (Button) findViewById(R.id.loginbtn);
-        Button signin = (Button) findViewById(R.id.signinbtn);
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SiginActivity.class);
-                startActivity(intent);
-            }
-        });
+        login = (Button) findViewById(R.id.loginbtn);
+        signin = (Button) findViewById(R.id.signinbtn);
+
+        mAuth = FirebaseAuth.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 em = email.getText().toString().trim();
                 pw = password.getText().toString().trim();
-
-                firebaseAuth.signInWithEmailAndPassword(em, pw)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(LoginActivity.this, ChooseActivity.class);
-                                    startActivity(intent);
-                                } else {
-                                    Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                if(em.length()==0 || pw.length()==0){
+                    Toast.makeText(LoginActivity.this, "ID/PW를 입력하세요.", Toast.LENGTH_SHORT).show();
+                }else{
+                    mAuth.signInWithEmailAndPassword(em, pw)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), ChooseActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                                    }
                                 }
-                            }
-                        });
+                            });
+                }
 
 
+
+            }
+        });
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SiginActivity.class);
+                startActivity(intent);
             }
         });
     }
