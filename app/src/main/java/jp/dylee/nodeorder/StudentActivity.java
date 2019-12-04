93f2f5573fca6e501ede1369146938d4b1cb2f95
JinsaGalbi <41094016+uncyclocity@users.noreply.github.com>
2019-12-05 08:24:22 +0900

    학생님 페이지, 선생님 페이지 나눔

---
+++ app/src/main/java/jp/dylee/nodeorder/StudentActivity.java
@@ -0,0 +1,16 @@
+package jp.dylee.nodeorder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StudentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { // 성범아 사랑해
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_main);
+
+    }
+
+
+}
