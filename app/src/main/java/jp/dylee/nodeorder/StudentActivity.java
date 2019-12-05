af71bcf09e4911c1c689949d41a40d565d4f728b
JinsaGalbi <41094016+uncyclocity@users.noreply.github.com>
2019-12-05 11:22:40 +0900

    학생 페이지, 선생님 페이지 나눔

--- app/src/main/java/jp/dylee/nodeorder/StudentActivity.java
+++ app/src/main/java/jp/dylee/nodeorder/StudentActivity.java
@@ -2,15 +2,33 @@ package jp.dylee.nodeorder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class StudentActivity extends Activity {
+    ArrayAdapter<CharSequence>  adspin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // 성범아 사랑해
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_main);
 
+        Spinner spinner = (Spinner) findViewById(R.id.spin_heya);
+        spinner.setPrompt("시/도 를 선택하세요.");
+
+        adspin = ArrayAdapter.createFromResource(this, R.array.heyas,    android.R.layout.simple_spinner_item);
+
+        adspin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adspin);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+            public void onNothingSelected(AdapterView<?>  parent) {
+            }
+        });
     }
-
-
 }
